2f5eb74adb5babf9eb3bab85bcbe188819695572
Guilherme Vieira <98032449+guilherme-hm-vieira@users.noreply.github.com>
2024-10-04 14:04:57 -0300

    feat: Remove comentário desnecessários

--- __main__.py
+++ __main__.py
@@ -5,16 +5,12 @@ import json
 def main():
     if len(sys.argv) < 2:
         print("Usage: python read_jwt.py <token>")
-        return
-    
-    # Pega o token do argumento da linha de comando
+        return    
+
     token = sys.argv[1]
     
     try:
-        # Decodifica o token sem verificar a assinatura
         decoded_token = jwt.decode(token, options={"verify_signature": False}, algorithms=["HS256"])
-
-        # Imprime o conteúdo decodificado em formato JSON identado
         print(json.dumps(decoded_token, indent=4))
 
     except jwt.InvalidTokenError:
